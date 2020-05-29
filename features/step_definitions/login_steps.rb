4388c2e4a7e26271fc01c4e9be751c1571822dd7
Gersonsouzace <gersonsouzace@gmail.com>
2020-05-29 16:10:54 -0300

    Implementando PageObject

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,8 +1,7 @@
 Quando("eu faço login com {string} e {string}") do |email, password|
-    visit "/"
-    find("#emailId").set email
-    find("#passId").set password
-    click_button "Entrar"
+    @login.go
+    @login.with(email, password)
+   
     sleep 3
 end
   
@@ -13,8 +12,7 @@ Entao("devo ser autenticado") do
 end
   
 Entao("devo ver {string} na área logada") do |expect_name|
-    user = find(' .sidebar-wrapper .user .info span')
-    expect(user.text).to eql expect_name
+    expect(@sidebar.logged_user).to eql expect_name
 end   
 
 Entao("não devo ser autenticado") do
@@ -24,6 +22,5 @@ Entao("não devo ser autenticado") do
 end
       
 Entao("devo ver a mensagem de alerta {string}") do |expect_message|
-    alert = find('.alert')
-    expect(alert.text).to eql expect_message
+    expect(@login.alert).to eql expect_message
 end
